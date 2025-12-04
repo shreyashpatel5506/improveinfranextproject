162b613fdf90c11b198a9bf3ba892070a923eaa2
Shreyash Patel <shreyashpatel5506@gmail.com>
2025-12-04 22:13:33 +0530

    create a officer api route is created

--- app/api/auth/createOfficer/route.js
+++ app/api/auth/createOfficer/route.js
@@ -2,4 +2,56 @@ import Officer from "@/app/model/Officer.model";
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import connectMongo from "@/app/db";
+import { NextResponse } from "next/server";
 
+const env = process.env.env
+const jwtSecret = process.env.JWTSECRET
+
+const generateToken = (officerId, res) => {
+    const token = jwt.sign({ officerId }, jwtSecret, {
+        expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: env !== "development",
+        sameSite: "strict",
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    return token;
+};
+
+export async function POST(req) {
+    try {
+        await connectMongo();
+        const { email, password } = req.body;
+
+        const exitinguser = await Officer.findOne({ email })
+        if (exitinguser) {
+            return NextResponse.json({ success: false, message: "user is already exiting by this email id" })
+        }
+
+        const hashedpassword = await bcrypt.hash(password, 10)
+        const newOfficer = new Officer({ email, password: hashedpassword })
+
+        await newOfficer.save()
+        const token = generateToken(newOfficer._id, res)
+        return NextResponse.json(
+            {
+                message: "Officer created ",
+                success: true,
+                Officer: newOfficer,
+                token
+            }, { status: 200 }
+        )
+    } catch (error) {
+        console.log("error fetchng in creation")
+        return NextResponse.json(
+            {
+                success: false,
+                message: "error to create a officer "
+            }, { status: 500 }
+        )
+    }
+}
